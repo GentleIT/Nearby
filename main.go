07c0303f9fcb4bb685cd4a4884fed83c7f01c7f8
CliqueGentle <nasyrovrahat28@gmail.com>
2026-03-08 19:09:43 +0500

    Polish Project

--- main.go
+++ main.go
@@ -1,48 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-)
-
-/*
-	Я хотел попробовать в слайс создать сотню юзеров и попробовать для каждого определить расстояние до следующего.
-	Их первоначальные позиции определялись бы рандомно.
-
-	Есть ли смысл сейчас пробовать это применять?
-	- Попробовать создать способ определения в реальном времени в каком квадрате находится User.
-	  Либо сделать это и попробовать сделать через while и switch case либо же как-то по другому.
-	- Но вообще я бы хотел создать всё же способ визуализации этих данных каким-нибудь способом.
-	  Пора ли? Но пока еще несколько функций на беке не было создано
-	- Что если создать систему квадратов и в этих квадратах юзер сможет открыть и прочитать посты которые написали
-	  Другие пользователи. Только как это все создать?
-
-	= Реализации системы зон в которой юзер находится и которая будет проверяться через while()
-*/
-
-// Как вообще оно будет само запускаться и всегда смотреть в какой зоне находиться юзер? Есть ли возможно создавать это
-// не для каждого юзера, а попробовать создать хендлер, который будет обрабатывать всех юзеров и возвращать им через
-// канал данные где они находятся?
-
-// func LocateZoneOfUser(users []User, wg *sync.WaitGroup) {
-// 	for true {
-// 		for _, v := range users {
-// 			switch {
-// 			case v.position.x > 0:
-// 				// Do something
-// 			case v.position.x < 0:
-// 				// Do something
-// 			default:
-// 				// Do something
-// 			}
-// 			fmt.Println(v)
-// 		}
-// 	}
-// 	wg.Done()
-// }
-
 func main() {
-	// timeStart := time.Now()
 
 	Area := struct {
 		width  int
@@ -52,46 +10,6 @@ func main() {
 		length: 16,
 	}
 
-	user1 := User{
-		name: "Herald",
-		position: Position{
-			x: 16,
-			y: 16,
-		},
-	}
-	user2 := User{
-		name: "Zero-Point",
-		position: Position{
-			x: 0,
-			y: 0,
-		},
-	}
-	users := []User{user1, user2, User{position: Position{x: 0, y: 8}}, User{position: Position{x: 2, y: 11}}, User{position: Position{x: rand.Intn(16), y: rand.Intn(16)}}}
-
-	// userss := []User{user1, user2}
-	// wg.Add(1)
-	// go LocateZoneOfUser(users, &wg)
-	// wg.Wait()
-	fmt.Printf("Location of User: x: %d, y: %d\n", user1.position.x, user1.position.y)
-	fmt.Printf("Location of Zero-Point: x: %d, y: %d\n", user2.position.x, user2.position.y)
-	fmt.Println("Result:")
-	fmt.Printf("Distance from zero-point: %v\n", CalculateDistance(&user1, &user2))
-	// fmt.Printf("In which direction located: %v\n", CalculateDirection(&user1, &user2))
-
-	a := "1ab4dcc84"
-	b := "1ab4dcc84fdgdfgr"
-	c := "1ab4ddc8"
-
-	fmt.Println(a, b, CheckSimilarity(a, b))
-	fmt.Println(b, c, CheckSimilarity(b, c))
-
-	for i, user := range users {
-		fmt.Printf("%v | x: %v, y: %v; hash: %s\n", i+1, user.position.x, user.position.y, HashUser(&user, Area.width, Area.length, 3))
-	}
-
-	// ---
-
-	// hashMap := make(map[string][]string)
 	hashMap := make(map[string][][]int)
 	for y := range Area.length {
 		for x := range Area.width {
@@ -102,11 +20,9 @@ func main() {
 		}
 	}
 
+	// Ниже буду получать соседей от хеша и далее помещать это в базу.
 	for i := 0; i <= 10; i++ {
 		// Поместить список существующих хешей и проверка их.
 		FindNeighborsForHash(hashMap, []int{1, 2, 3})
 	}
-
-	// fmt.Println(hashMap, time.Since(timeStart))
-	// fmt.Println(hashMap, "=======", hashMap["aaa"])
 }
