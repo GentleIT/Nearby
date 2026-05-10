333e90f380874c6a56397a585aa34fd276405eef
CliqueGentle <nasyrovrahat28@gmail.com>
2026-05-10 22:33:22 +0500

    feat(db): Add GetPostsFromDb() + SendPostToDb()

--- db.go
+++ db.go
@@ -20,11 +20,38 @@ func ConnectToDB() *sql.DB {
 	return db
 }
 
-func SendPostToDb(db *sql.DB, post *Post) {
+func SendPostToDb(post *Post) {
+	db := ConnectToDB()
+	defer db.Close()
+
 	query := "INSERT INTO posts (hash, owner_name, title, content) VALUES ($1, $2, $3, $4) RETURNING id"
-	v, err := db.Exec(query, post.hash, post.owner.name, post.title, post.content)
+	id, err := db.Exec(query, post.hash, post.owner, post.title, post.content)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(v)
+	fmt.Println(id)
+} // Should I somehow save id in some map? But this map doesnt make any sense if it will wipe itself after every run
+// Or should I return post id to save this id in another column or some other data variant to save posts belonging to user?
+
+func GetPostsFromDb(db *sql.DB) ([]Post, error) {
+	query := "SELECT * FROM posts"
+	posts := make([]Post, 0, 100)
+
+	rows, err := db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		postRow := Post{}
+		if err := rows.Scan(&postRow.id, &postRow.hash, &postRow.created_at, &postRow.owner, &postRow.title, &postRow.content); err != nil {
+			return nil, err
+		}
+
+		posts = append(posts, postRow)
+	}
+
+	return posts, nil
 }
